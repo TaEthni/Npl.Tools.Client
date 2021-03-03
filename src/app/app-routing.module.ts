7eaa53e27417517c1c3592a1e091546b226f785b
Joel Cox <joel.cox.dev@gmail.com>
2021-03-02 20:29:55 -0600

    chore: update router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     {
         path: '',
         component: ToolsComponent
+    },
+    {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: ''
     }
 ];
 
